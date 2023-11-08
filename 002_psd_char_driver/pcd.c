48777b4fb3e0c3ff0f843d006c311794a857606f
Mehmet Alinbay <mehmet.alinbay@pavotek.com.tr>
2023-11-08 16:42:38 +0300

    PCD surucusune Read/Write/Lseek fonksiyonlari eklendi.

--- 002_psd_char_driver/pcd.c
+++ 002_psd_char_driver/pcd.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/uaccess.h>
 
 #define DEV_MEM_SIZE 512
 
@@ -19,19 +20,92 @@ struct class *class_pcd;
 /* Device variable */
 struct device *device_pcd;
 
-loff_t pcd_lseek(struct file *fp, loff_t off, int whence)
+loff_t pcd_lseek(struct file *fp, loff_t offset, int whence)
 {
-    return 0;
+    loff_t temp;
+
+    pr_info("lseek requested \n");
+    pr_info("Current file position is : %lld\n",fp->f_pos);
+
+    switch(whence)
+    {
+        case SEEK_SET:
+            if((offset > DEV_MEM_SIZE) || (offset < 0))
+            {
+                return -EINVAL;
+            }
+            fp->f_pos = offset;
+            break;
+        case SEEK_CUR:
+            temp = fp->f_pos + offset;
+            if((temp > DEV_MEM_SIZE) || (temp < 0))
+            {
+                return -EINVAL;
+            }
+            fp->f_pos = temp;
+            break;
+        case SEEK_END:
+            temp = DEV_MEM_SIZE + offset;
+            if((temp > DEV_MEM_SIZE) || (temp < 0))
+            {
+                return -EINVAL;
+            }
+            fp->f_pos = temp;
+            break;
+        default:
+            return -EINVAL;
+    }
+
+    pr_info("New Value of the file position = %lld\n", fp->f_pos);
+    return fp->f_pos;
 }
 
 ssize_t pcd_read(struct file *fp, char __user *buff, size_t count, loff_t *f_pos)
 {
-    return 0;
+    pr_info("Read request for %zu bytes\n",count);
+    /* Adjust the count */
+    if((*f_pos)+count > DEV_MEM_SIZE)
+    {
+        count = DEV_MEM_SIZE - (*f_pos);
+    }
+    /* Copy to user */
+    if(copy_to_user(buff, &device_buffer[*f_pos], count))
+    {
+        return -EFAULT;
+    }
+    /* Update the file postition */
+    *f_pos += count;
+    pr_info("Number of bytes read = %zu \n",count);
+    pr_info("Updated file position = %lld \n",*f_pos);
+    return count;
 }
 
 ssize_t pcd_write(struct file *fp, const char __user *buff, size_t count, loff_t *f_pos)
 {
-    return 0;
+    pr_info("Write request for %zu bytes\n",count);
+    pr_info("Current file position is : %lld\n",fp->f_pos);
+    
+    /* Adjust the count */
+    if((*f_pos) + count > DEV_MEM_SIZE)
+    {
+        count = DEV_MEM_SIZE - (*f_pos);
+    }
+    
+    if(!count)
+    {
+        pr_err("Error: No Memory\n");
+        return -ENOMEM;
+    }
+    /* Copy from user */
+    if(copy_from_user(&device_buffer[*f_pos],buff,count))
+    {
+        return -EFAULT;
+    }
+    /* Update the file position */
+    *f_pos += count;
+    pr_info("Number of bytes written = %zu \n",count);
+    pr_info("Updated file position = %lld \n",*f_pos);
+    return count; 
 }
 
 int pcd_open(struct inode *inode, struct file *fp)
@@ -77,8 +151,8 @@ static int __init psd_driver_init(void)
     /*6 Create device and Populate sysfs */
     device_pcd = device_create(class_pcd,NULL,device_number,NULL,"pcd");
 
-    pr_info("PCD Module init was successful\n");
-    pr_info("Device Number <major>:<minor>=%d:%d\n",MAJOR(device_number),MINOR(device_number));
+    pr_info("PCD Module init was successful \n");
+    pr_info("Device Number <major>:<minor>=%d:%d \n",MAJOR(device_number),MINOR(device_number));
 
     return 0;
 }
@@ -97,9 +171,9 @@ static void __exit psd_driver_exit(void)
 
     /*4 Unregister */
     unregister_chrdev_region(device_number,1);
-    
+
     pr_info("PCD Module cleanup, done\n"); 
-    
+ 
 }
 
 /* Registration Points */
